3e89e46a5918d59475851f9d4f1558956c110d38
Nick Watters <nwatters@mit.edu>
2021-06-09 22:31:02 -0400

    Update package version number in setup.py after newest distribution to pypi.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='moog-games',
-    version='1.3',
+    version='1.4',
     description=(
         'Modular object oriented games utils is a python-based game engine.'),
     author='Nicholas Watters',
